3fa771aea1a5ff57cb8efed37ce270d7d7ee87bc
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2023-08-25 16:58:41 +0300

    V3 Additional page, nav icons replace text

--- App.js
+++ App.js
@@ -5,16 +5,36 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen'
 import Page2 from './Page2'
 import Page3 from './Page3'
+import Page4 from './Page4'
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ tabBarIconStyle: { display: "none" } }}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({ // Navigator can be customized using screenOptions
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'md-home';
+            } else if (route.name === 'Add') {
+              iconName = 'md-add-circle-sharp';
+            } else if (route.name === 'Plotter') {
+              iconName = 'easel';
+            } else if (route.name === 'Settings') {
+              iconName = 'md-settings';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />; //it returns an icon component
+          },
+        })}>
+
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Page 2" component={Page2} />
-        <Tab.Screen name="Page 3" component={Page3} />
+        <Tab.Screen name="Add" component={Page2} />
+        <Tab.Screen name="Plotter" component={Page3} />
+        <Tab.Screen name="Settings" component={Page4} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ Page4.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import styles from './Styles'
+
+export default function Page4() {
+  return (
+    <View style={styles.container}>
+      <Text>Page 4</Text>
+    </View>
+  );
+}
