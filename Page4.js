b52756e1dff53fdb16b5fad4d43bb50f1993d5dc
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2023-08-27 13:34:46 +0300

    V6 Settings page content established.

--- Page4.js
+++ Page4.js
@@ -1,13 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 import styles from './Styles'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 export default function Page4() {
+
+  let [theme, setTheme] = useState("Light");
+  let [switchicon, setSwitchIcon] = useState("lightbulb-on");
+
+  const themeButtonPressed = () => {
+    if (theme === "Light") {
+      setTheme("Dark");
+      setSwitchIcon("lightbulb-off");
+    } else {
+      setTheme("Light");
+      setSwitchIcon("lightbulb-on");
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Tutorial</Text>
-      <Text>Theme</Text>
-      <Text>Language</Text>
+
+      <View style={{
+        flex: 1, flexDirection: 'row',
+        alignItems: 'center'
+      }}>
+        <Text>Tutorial</Text>
+        <Ionicons.Button name="link" size={24} color="black" />
+      </View>
+
+      <View style={{
+        flex: 1, flexDirection: 'row',
+        alignItems: 'center'
+      }}>
+        <Text>Theme</Text>
+        <MaterialCommunityIcons.Button name={switchicon} size={24} color="black" onPress={themeButtonPressed} />
+      </View>
+
+      <View style={{
+        flex: 1, flexDirection: 'row',
+        alignItems: 'center'
+      }}>
+        <Text>Language</Text>
+        <Button title="English" />
+        <Button title="Suomi" />
+      </View>
+
       <Text>About</Text>
+      <Text>This app was created in 2023 by Kristopher Pepper. The app started out as a school project but...</Text>
     </View>
   );
 }
