b2ebbd3c4db73ae067149bbc4be1105ccc6a9044
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2023-09-01 17:13:43 +0300

    V12 SQLite database added and integrated into project

--- Page2.js
+++ Page2.js
@@ -1,7 +1,10 @@
-import { StyleSheet, Text, View, Button, Alert, TextInput, ScrollView } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput, ScrollView, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
 import styles from './Styles'
 import Ionicons from '@expo/vector-icons/Ionicons';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('companiesdb.db');
 
 export default function Page2() {
 
@@ -9,14 +12,67 @@ export default function Page2() {
   const apiKey = "cl7Ia65FhThK_ldjqiazYEB_qK4yhlFe";
   const [result, setResult] = useState(null);
   const [companies, setCompanies] = useState([]);
-  
+
+  // Creates a table if one does not exist already
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists companies (id integer primary key not null, name text, ticker text);');
+    }, null, updateList);
+  }, []);
+
+  // Update company list
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from companies;', [], (_, { rows }) =>
+        setCompanies(rows._array)
+      );
+    });
+  }
+
+  // Delete company
+  const deleteItem = (id) => {
+    db.transaction(
+      tx => {
+        tx.executeSql(`delete from companies where id = ?;`, [id]);
+      }, null, updateList
+    )
+  }
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 5,
+          width: "80%",
+          backgroundColor: "#fff",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
+  // Save course
+  const saveItem = (name, ticker) => {
+    db.transaction(tx => {
+      tx.executeSql('insert into companies (name, ticker) values (?, ?);', [name, ticker]);
+    }, null, updateList
+    )
+  }
+
   const fetchData = async (input) => {
     try {
-      let url = 'https://api.polygon.io/v3/reference/tickers/' + input.toUpperCase() + '?apiKey=' + apiKey;
-      const response = await fetch(url);
-      const jsonData = await response.json();
-      setResult(jsonData);
-      setCompanies([...companies, { name: jsonData.results.name, ticker: jsonData.results.ticker }]);
+      if (input.trim() !== "") {
+        let url = 'https://api.polygon.io/v3/reference/tickers/' + input.toUpperCase() + '?apiKey=' + apiKey;
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setResult(jsonData);
+
+        const companyName = jsonData.results.name;
+        const companyTicker = jsonData.results.ticker;
+
+        setCompanies([...companies, { name: companyName, ticker: companyTicker }]);
+        saveItem(companyName, companyTicker); // Save fetched data to the database
+      }
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -35,11 +91,14 @@ export default function Page2() {
         <Ionicons.Button name="search" size={24} color="black" onPress={() => fetchData(searchinput)} />
       </View>
 
-      <ScrollView>
-        {companies.map((company, index) => (
-          <Text key={index}>{company.name + " " + company.ticker}</Text>
-        ))}
-      </ScrollView>
+      <FlatList
+        style={{ marginLeft: "5%" }}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => <View style={styles.listcontainer}><Text style={{ fontSize: 18 }}>{item.name}, {item.ticker}</Text>
+          <Text style={{ fontSize: 18, color: '#0000ff' }} onPress={() => deleteItem(item.id)}> Delete</Text></View>}
+        data={companies}
+        ItemSeparatorComponent={listSeparator}
+      />
 
       <View style={{
         flex: 1, flexDirection: 'row',
