45a423c78eefece3c9b5d91ed869053650d43ab6
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2023-10-15 18:33:45 +0300

    V31, BoardPage: notes are now added to the table

--- Pages/BoardPageFunctions/AddNote.js
+++ Pages/BoardPageFunctions/AddNote.js
@@ -1,6 +1,16 @@
+import { insertNote} from '../../Databases/BoardDatabase';
+
 // Add a note to the Board
-export const addNote = (notes, setNotes) => {
+export const addNote = (notes, setNotes, text) => {
     console.log('Add note function called');
+
+    insertNote("New Note.", 320, 340)
+        .then(() => {
+          console.log('Note added to table.');
+        })
+        .catch((error) => {
+          console.error(`Error saving note to table: ${error}`);
+        });
 
     const newNote = {
         id: notes.length + 1,
