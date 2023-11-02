5c1298175c103f70e6b0ba1ab843c3da09df6b2b
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2023-11-02 21:43:27 +0200

    V38, board page external function files scrutinized

--- Pages/BoardPageFunctions/MoveNote.js
+++ Pages/BoardPageFunctions/MoveNote.js
@@ -1,25 +1,23 @@
 import { updateNote } from '../../Databases/BoardDatabase';
 
-// The note movement
+// The note gesture movement
 export const moveNote = (event, noteId, prevNotes, setNotes) => {
   const { translationX, translationY } = event.nativeEvent;
 
   const updatedNotes = prevNotes.map((note) => {
     if (note.id !== noteId) return note;
-
+    // Note movement speed brakes
     const panningSpeed = 0.065;
-    // Calculate new positions
+    // Calculate new positions. Current postion + new position value
     let newXPosition = note.x + (translationX * panningSpeed);
     let newYPosition = note.y + (translationY * panningSpeed);
-
-    // Set boundaries for the new positions
+    // Set board boundaries for the new positions
     // Left and top side
     newXPosition = Math.max(0, newXPosition);
     newYPosition = Math.max(0, newYPosition);
     // Right and bottom side
     newXPosition = Math.min(580, newXPosition);
     newYPosition = Math.min(915, newYPosition);
-
     const text = note.text;
 
     // Update the note in the database
@@ -37,6 +35,6 @@ export const moveNote = (event, noteId, prevNotes, setNotes) => {
       y: newYPosition,
     };
   });
-
+  // Update the notes list
   setNotes(updatedNotes);
 };
--- Pages/BoardPageFunctions/UpdateNoteHandler.js
+++ Pages/BoardPageFunctions/UpdateNoteHandler.js
@@ -1,12 +1,12 @@
-// Edit an existing note
 import { updateNote } from '../../Databases/BoardDatabase';
 
+// Edit an existing note
 export const updateNoteHandler = (text, noteId, notes, setNotes) => {
     const updatedNotes = notes.map((note) => {
         if (note.id === noteId) {
             const updatedNote = { ...note, text };
 
-            // Update the note in the database
+            // Update the note in the table
             updateNote(noteId, text, note.x, note.y)
                 .then(() => {
                     //console.log('Note updated in table.'); //Testing only
@@ -14,7 +14,7 @@ export const updateNoteHandler = (text, noteId, notes, setNotes) => {
                 .catch((error) => {
                     console.error(`Error updating note in table: ${error}`);
                 });
-
+            // The note is saved to screen. Different from saving to table
             return updatedNote;
         } else {
             return note;
